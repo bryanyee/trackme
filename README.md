b32008bbe76c1d785e5050ae81f2a7ba8c37ddc2
Bryan Yee <yee.bryan.c@gmail.com>
2022-05-14 09:48:35 -0700

    Add branch button to tracker

---
+++ README.md
@@ -0,0 +1,3 @@
+### Trackme
+
+- On a Pivotal Tracker story page, adds a button to automatically generate a branch name and copy it to the clipboard.
---
+++ pivotalTracker.js
@@ -0,0 +1,60 @@
+
+function createHtmlContent() {
+  const htmlContent = document.createElement('div');
+  htmlContent.setAttribute('id', 'branch-name-section');
+  htmlContent.style.marginBottom = '10px';
+  htmlContent.style.width = '100%';
+  htmlContent.innerHTML = `<button id="branch-name-button" style="border: 1px solid #CCC; border-radius: 2px; cursor: pointer; padding: 2px; width: 100%;">Branch name</button>`;
+  return htmlContent;
+}
+
+// Branch name convention:
+// <tracker-id>/<story-type>/<story-description-text>
+// e.g. 123456789/feature/hello-my-feature
+
+function generateBranchName() {
+  const rawTitle = document.querySelector('[aria-label="story title"]').value;
+  const smallWords = ['a', 'the', 'an'];
+  const titleValue = rawTitle.toLowerCase().split(/[^a-zA-Z\d]/).filter((word) => !smallWords.includes(word)).slice(0, 7).join('-');
+  const rawId = document.querySelector('[aria-label="story id"]').value;
+  const idValue = rawId.slice(1);
+  const storyTypeValue = document.querySelector('[name="story[story_type]"]').value;
+  return `${idValue}/${storyTypeValue}/${titleValue}`;
+}
+
+function copyBranchNameToClipboard() {
+  const branchName = generateBranchName();
+  navigator.clipboard.writeText(branchName);
+
+  const button = document.querySelector('#branch-name-button');
+  button.textContent = 'Copied to clipboard!';
+  setTimeout(() => {
+    button.textContent = 'Branch name';
+  }, 2000);
+}
+
+function appendBranchNameButton() {
+  // Create the html containing the button
+  const htmlContent = createHtmlContent();
+
+  // Add a click event handler, to copy a generated branch name to the clipboard
+  htmlContent.addEventListener('click', function(e) {
+    e.preventDefault();
+    copyBranchNameToClipboard();
+  });
+
+  // Insert the html into Tracker page
+  const navSection = document.querySelector('nav.edit');
+  navSection.after(htmlContent);
+}
+
+function start() {
+  const interval = setInterval(function() {
+    if (document.querySelector('.project') !== null) {
+      appendBranchNameButton();
+      clearInterval(interval);
+    }
+  }, 1000);
+}
+
+start();
